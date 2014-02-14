7eee614489feb7a9341692444495fcd83bcf959c
Kalimehtar <kalimehtar@mail.ru>
2014-02-14 23:15:34 +0600

    Fixed compile without X

--- gtk/loadlib.lisp
+++ gtk/loadlib.lisp
@@ -7,16 +7,6 @@
 
 (in-package :gtk-cffi)
 
-;; ;; Added possibility of using list of enums as sum 
-;; (defmethod cffi::translate-type-to-foreign (value (type cffi::foreign-enum))
-;;   (cond
-;;    ((keywordp value) (cffi::%foreign-enum-value type value))
-;;    ((listp value)
-;;     (apply '+ (mapcar
-;;                (lambda (x) (cffi::translate-type-to-foreign x type))  value)))
-;;    (t value)))
-
-
 (eval-when (:compile-toplevel :load-toplevel)
   (unless (find :gtk *features*)
     (push :gtk *features*)
@@ -27,14 +17,6 @@
     (use-foreign-library :gtk)))
 
 (eval-when (:compile-toplevel)
-  (defcfun ("gtk_init" %gtk-init) :void (argc :pointer) (argv :pointer))
-
-  #+sbcl (sb-ext::set-floating-point-modes :traps nil)
-  (with-foreign-objects ((argc :int) (argv :pointer))
-    (setf (mem-ref argc :int) 0
-          (mem-ref argv :pointer) (foreign-alloc :string 
-                                                 :initial-element "program"))
-    (%gtk-init argc argv))
   (defcfun gtk-get-major-version :uint)
   (defcfun gtk-get-minor-version :uint)
   (when (and (>= (gtk-get-major-version) 3) (>= (gtk-get-minor-version) 2))
